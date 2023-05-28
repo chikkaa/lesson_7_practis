21906fe7e18534faf217ba40bd597f420f4d5b6c
chikkaa <turgunbaevcyngyz1@gmail.com>
2023-05-28 16:12:32 +0300

    first commit

---
+++ lesson_7_practis.dart
@@ -0,0 +1,75 @@
+void main() {
+  //task1
+//   int a = 1;
+
+//   while (a <= 100) {
+//     print(a);
+//     a++;
+//   }
+
+//   for (int a = 0; a <= 100; a++) {
+//     print(a);
+//   }
+
+// //task2
+//   int b = 11;
+
+//   do {
+//     print(b);
+//     b++;
+//   } while (b <= 33);
+
+//task3
+  // for (int i = 0; i <= 100; i += 2) {
+  //   print(i);
+  // }
+
+  // int c = 0;
+  // // while (c <= 100) {
+  // //   print(c);
+  // //   c += 2;
+  // // }
+
+//   for (int i = 0 ; i <= 100; i++) {
+//    if (i % 2 ==0){
+//    print(i);
+//    }
+//   }
+
+//   while(c <= 100){
+//    if (c % 2 ==0){
+//     print(c);
+//    }
+//    c++;
+//   }
+
+//   for (int i = 0; i >= 100; i++){
+//     if (i.isEven){
+//     print(i);
+//   }
+// }
+
+// while(c <= 100){
+//   if (c.isEven){
+//     print(c);
+//   }
+//   c++;
+// }
+
+
+//task4
+int b =0;
+int s= 0;
+while(s <=100){
+ b= b + s;
+  s++;
+}
+print (b);
+
+int a =0;
+for (int i =0; i <= 100; i++){
+a = i + a;
+
+}
+print (a);
+}
